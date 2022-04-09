f320768db5ccedf3998c00a9536d2bb4e780373a
wangchaoli <15728005026@163.com>
2022-04-09 14:56:11 +0800

    feat: 修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/select-better/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/deploy-my-demo/' : '/',
 }
